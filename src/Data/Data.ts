d20180a1104791bdebad1c768bfb6d7049f39492
faris <mrfaristp@gmail.com>
2024-03-03 12:55:57 +0530

    about section completed

--- src/Data/Data.ts
+++ src/Data/Data.ts
@@ -11,3 +11,38 @@ export const socialLinks = [
   { url: "https://www.linkedin.com/in/faris-tp", image: "/linkedin.png" },
   { url: "https://medium.com/@faristp", image: "/medium.png" },
 ];
+
+export const skills = [
+  "Javascript",
+  "typescript",
+  "HTML",
+  "CSS",
+  "TailwindCSS",
+  "Framer motion",
+  "NextJS",
+  "React JS",
+  "NodeJS",
+  "expressJS",
+  "MongoDB",
+  "AWS",
+  "Github",
+  "Rest API",
+  "Data struture",
+];
+
+export const jobData = [
+  {
+    jobTitle: "Full Stack Developer",
+    jobDescription:
+      "Joined Diffrenz as a full stack developer, contributing to team projects and collaborating with colleagues.",
+    jobDate: "2024 - Present",
+    jobCompany: "Diffrenz",
+  },
+  {
+    jobTitle: "Freelancer",
+    jobDescription:
+      "As a freelancer, I worked on various web development projects, gaining valuable experience and skills.",
+    jobDate: "2021 - 2023",
+    jobCompany: "Self-Employed",
+  },
+];
